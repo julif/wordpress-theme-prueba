59afcd574565e7c30fb3f717ad3c400f93248e37
Julif <julif-@live.com>
2020-03-08 10:12:51 -0300

    v 0.4
    estilo basico

--- theme/single.php
+++ theme/single.php
@@ -1,3 +1,10 @@
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Blog de Prueba</title>
+  <!-- css -->
+  <link rel="stylesheet" href="<?php bloginfo('template_url')?>/style.css" type="text/css" media="screen" /><!-- hoja de estilos pricipal -->
+</head>
 <?php get_header() ?>
 <main>
 <?php  if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
